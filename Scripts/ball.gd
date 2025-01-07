b7b93341ad92a89c043a980da14412bc8707e749
Cameron Johnston <cameron.robin.johnston@gmail.com>
2025-01-07 12:04:55 +0000

    added comments

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Random velocity, should change to variable later
 	apply_central_impulse(Vector2(-414,212))
 
 
