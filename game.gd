f4132c691ef1c94028b0167649b69683a87e07e6
Doruk Balci <doruk.balci@york.ac.uk>
2025-01-08 16:25:26 +0000

    wiggle

--- game.gd
+++ game.gd
@@ -1,45 +1,48 @@
 extends Node2D
 
 @export var MAX_LINES = 2
+var wiggle_timer = 0.0
+@export var wiggle_speed = 10.0  # Speed of the wiggle
+@export var wiggle_amount = 0.1  # Amount of the wiggle (rotation in radians)
 
 # Dictionary mapping input actions to positions
 @onready var action_positions := {
-	"Q": $"Positions/Q".position,
-	"W": $"Positions/W".position,
-	"E": $"Positions/E".position,
-	"R": $"Positions/R".position,
-	"T": $"Positions/T".position,
-	"Y": $"Positions/Y".position,
-	"U": $"Positions/U".position,
-	"I": $"Positions/I".position,
-	"O": $"Positions/O".position,
-	"P": $"Positions/P".position,
-	"A": $"Positions/A".position,
-	"S": $"Positions/S".position,
-	"D": $"Positions/D".position,
-	"F": $"Positions/F".position,
-	"G": $"Positions/G".position,
-	"H": $"Positions/H".position,
-	"J": $"Positions/J".position,
-	"K": $"Positions/K".position,
-	"L": $"Positions/L".position,
-	"Z": $"Positions/Z".position,
-	"X": $"Positions/X".position,
-	"C": $"Positions/C".position,
-	"V": $"Positions/V".position,
-	"B": $"Positions/B".position,
-	"N": $"Positions/N".position,
-	"M": $"Positions/M".position,
-	"1": $"Positions/1".position,
-	"2": $"Positions/2".position,
-	"3": $"Positions/3".position,
-	"4": $"Positions/4".position,
-	"5": $"Positions/5".position,
-	"6": $"Positions/6".position,
-	"7": $"Positions/7".position,
-	"8": $"Positions/8".position,
-	"9": $"Positions/9".position,
-	"0": $"Positions/0".position
+	"Q": $"Positions/Q",
+	"W": $"Positions/W",
+	"E": $"Positions/E",
+	"R": $"Positions/R",
+	"T": $"Positions/T",
+	"Y": $"Positions/Y",
+	"U": $"Positions/U",
+	"I": $"Positions/I",
+	"O": $"Positions/O",
+	"P": $"Positions/P",
+	"A": $"Positions/A",
+	"S": $"Positions/S",
+	"D": $"Positions/D",
+	"F": $"Positions/F",
+	"G": $"Positions/G",
+	"H": $"Positions/H",
+	"J": $"Positions/J",
+	"K": $"Positions/K",
+	"L": $"Positions/L",
+	"Z": $"Positions/Z",
+	"X": $"Positions/X",
+	"C": $"Positions/C",
+	"V": $"Positions/V",
+	"B": $"Positions/B",
+	"N": $"Positions/N",
+	"M": $"Positions/M",
+	"1": $"Positions/1",
+	"2": $"Positions/2",
+	"3": $"Positions/3",
+	"4": $"Positions/4",
+	"5": $"Positions/5",
+	"6": $"Positions/6",
+	"7": $"Positions/7",
+	"8": $"Positions/8",
+	"9": $"Positions/9",
+	"0": $"Positions/0"
 }
 
 # Store active actions to draw lines
@@ -48,10 +51,20 @@ var active_actions: Array
 # Store active lines
 var lines: Array
 
+var _delta : float
+
+func _physics_process(delta: float) -> void:
+	_delta = delta
+	for action in active_actions:
+		wiggle_timer += _delta * wiggle_speed
+		action_positions[action].rotation = sin(wiggle_timer) * wiggle_amount
+		
+
 func _input(event):
 	for action in action_positions.keys():
 		if event.is_action_pressed(action) and action not in active_actions:
 			add_point(action)
+			
 		elif event.is_action_released(action) and action in active_actions:
 			remove_point(action)
 			
@@ -59,8 +72,8 @@ func _input(event):
 func add_point(action: String):
 	active_actions.append(action)
 	if active_actions.size() % 2 == 0 and lines.size() < MAX_LINES:
-		var start_pos = action_positions[active_actions[-1]]
-		var end_pos = action_positions[active_actions[-2]]
+		var start_pos = action_positions[active_actions[-1]].position
+		var end_pos = action_positions[active_actions[-2]].position
 		
 		var line = Line.new_line(start_pos, end_pos,[active_actions[-1], active_actions[-2]])
 		add_child(line)
@@ -72,7 +85,7 @@ func sounds_play_sound(keys: Array):
 
 func remove_point(action: String):
 	active_actions.erase(action)
-	var action_position = action_positions[action]
+	var action_position = action_positions[action].position
 	for line in lines:
 		var ix = line.get_points().find(action_position)
 		if ix != -1:
