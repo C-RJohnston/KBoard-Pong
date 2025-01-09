d1cca301443e0ec6617d8f10b8c3485cf4abe690
Cameron Johnston <cameron.robin.johnston@gmail.com>
2025-01-09 11:36:36 +0000

    added line particles and ball speed

--- DorukZone B-)/make_linesproto.gd
+++ DorukZone B-)/make_linesproto.gd
@@ -12,12 +12,16 @@ static func new_line(start_pos: Vector2, end_pos: Vector2, keys: Array):
 	var line: Line = scene.instantiate()
 	var line2d = line.get_child(0)
 	var collision = line.get_child(1)
+	var particles = line.get_child(2)
 	line2d.add_point(start_pos)
 	line.start = start_pos
 	line2d.add_point(end_pos)
 	line.end = end_pos
 	line.keys = keys
 	collision.polygon = [start_pos, end_pos]
+	particles.position = (start_pos + end_pos) / 2
+	particles.rotation = atan((end_pos.y - start_pos.y)/(end_pos.x-start_pos.x))
+	particles.emission_rect_extents = Vector2((end_pos-start_pos).length()/2, 10)
 	return line
 
 func get_points():
--- Scenes/Ball/ball.gd
+++ Scenes/Ball/ball.gd
@@ -1,5 +1,9 @@
 extends RigidBody2D
 
+# Speeds of the ball
+@export var MAX_SPEED = 1000
+@export var MIN_SPEED = 100
+@export var DEFAULT_SPEED = 400
 # Maximum number of points in the trail
 @export var max_points: int = 50
 @onready var initial_position = position
@@ -8,10 +12,22 @@ var queue: Array
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Random velocity, should change to variable later
-	apply_central_impulse(Vector2(400,0))
+	set_axis_velocity(Vector2(DEFAULT_SPEED,0))
 	line.clear_points()
 	queue.clear()
 
+func increase_speed(amount: float):
+	var speed = linear_velocity.length()
+	var unit = linear_velocity / speed
+	var new_speed = min(MAX_SPEED, speed + amount)
+	set_axis_velocity(unit * new_speed)
+
+func decrease_speed(amount: float):
+	var speed = linear_velocity.length()
+	var unit = linear_velocity / speed
+	var new_speed = max(MIN_SPEED, speed - amount)
+	print(new_speed)
+	set_axis_velocity(unit * new_speed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,6 +39,11 @@ func _process(delta):
 	for point in queue:
 		line.add_point(point)
 
+func _input(event):
+	if(event.is_action_pressed("ui_up")):
+		increase_speed(50)
+	if(event.is_action_pressed("ui_down")):
+		decrease_speed(50)
 
 
 func _on_body_entered(body):
